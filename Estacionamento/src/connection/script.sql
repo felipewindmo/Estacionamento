4e8e7f191a495e97ff1cb24151472157be460e7a
03357081024 <03357081024@LAB205-1019992.fw.iffar.edu.br>
2022-10-27 13:12:01 -0300

    semana 4 crud cadastro vaga finalização crud vaga

--- Estacionamento/src/connection/script.sql
+++ Estacionamento/src/connection/script.sql
@@ -11,3 +11,15 @@ PRIMARY KEY (idVaga));
 
 )
 
+CREATE TABLE motorista(
+idMoto int NOT NULL AUTO_INCREMENT,
+nome varchar (150) NOT NULL,
+genero varchar(30) NOT NULL,
+cpf int(11) NOT NULL,
+rg int(8) NOT NULL,
+celular int(11) NOT NULL,
+email varchar(200) NOT NULL,
+senha varchar(32) NOT NULL,
+PRIMARY KEY (idMoto));
+
+)
---
+++ Estacionamento/src/model/dao/MotoristaDAO.java
@@ -0,0 +1,31 @@
+package model.dao;
+import model.bean.Motorista;
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+public class MotoristaDAO {
+    public void create(Motorista m){
+    Connection con = ConnectionFactory.getConnection();
+    PreparedStatement stmt = null;
+    
+    try{
+    stmt = con.prepareStatement("INSERT INTO motorista (Nome, Email, Senha, Rg, Cpf, Genero, Celular )VALUES (?, ?, ?, ?, ?, ?, ?)");
+    stmt.setInt(1, m.getRg());
+    stmt.setString(2, m.getNome());
+    stmt.setString(3, m.getEmail());
+    stmt.setString(4, m.getSenha());
+    stmt.setString(5, m.getGenero());
+    stmt.setInt(6, m.getCelular());
+    stmt.setInt(2, m.getCpf());
+    stmt.executeUpdate();
+    JOptionPane.showMessageDialog(null, "Motorista salvo com sucesso!");
+    }catch (SQLException e){
+    JOptionPane.showMessageDialog(null, "Erro ao salvar: " + e);
+    }finally{
+   ConnectionFactory.closeConnection(con, stmt);
+    }
+    
+    }
+}
